d26303f9394e489225ca04648932f0f377189e15
N1ckn1ght <thisefi@yandex.ru>
2025-10-29 16:51:31 +0800

    indev 251029

--- src/board/magic.rs
+++ src/board/magic.rs
@@ -1,30 +1,39 @@
 // seed: &mut u32
 
-use crate::util::set_bit;
+use crate::util::{del_bit, get_bit, pop_bit, set_bit};
 
 fn search_for_magic(sq: usize) {
     let mut combs: Vec<u64> = vec![0; 256];
     
 }
 
-fn init_blocker_boards() -> [u64; 256] {
-    let mut bbs = [0; 256];
-    for (i, bb) in bbs.iter_mut().enumerate() {
-        let mut buf;
+// todo: also precompute attack bit handles? not magic, just bitx, bity -> changes
+
+fn init_attacks(sq: usize, combinations: &[u64; 256]) -> [u64; 256] {
+    let mut attacks = [0; 256];
+    for i in 0..256 {
+        let mut bb = combinations[i];
+        let mut alt;
+
         // up
-        buf = 0;
-        for j in (i..36).step_by(6) {
-            if buf == 0 {
-                buf = j;
+        alt = false;
+        for j in (i+6..36).step_by(6) {
+            alt = !alt;
+            if alt {
+                if !get_bit(bb, j) {
+                    break;
+                }
             } else {
-                set_bit(bb, j);
-                set_bit(bb, buf);
-                buf = 0;
+                if get_bit(bb, j) {
+                    break;
+                }
+
             }
         }
+
         // right
         buf = 0;
-        for j in i..((i / 6 + 1) * 6) {
+        for j in i+1..((i / 6 + 1) * 6) {
             if buf == 0 {
                 buf = j;
             } else {
@@ -33,26 +42,112 @@ fn init_blocker_boards() -> [u64; 256] {
                 buf = 0;
             }
         }
+
         // down
-        buf = 0;
-        for j in (0..=i).rev().step_by(6) {
-            if buf == 0 {
-                buf = j;
-            } else {
-                set_bit(bb, j);
-                set_bit(bb, buf);
+        if i > 11 {
+            buf = 0;
+            for j in (0..i-5).rev().step_by(6) {
+                if buf == 0 {
+                    buf = j;
+                } else {
+                    set_bit(bb, j);
+                    set_bit(bb, buf);
+                    buf = 0;
+                }
             }
         }
+
         // left
         buf = 0;
-        for j in (i/6*6..=i).rev() {
+        for j in (i/6*6..i).rev() {
             if buf == 0 {
                 buf = j;
             } else {
                 set_bit(bb, j);
                 set_bit(bb, buf);
+                buf = 0;
             }
         }
+
+        del_bit(bb, i);
+    }
+
+    attacks
+}
+
+fn init_combinations(bb: u64) -> [u64; 256] {
+    let mut combs = [0; 256];
+    for (i, comb) in combs.iter_mut().enumerate() {
+        let mut mask = bb;
+        let mut bit = 0;
+        while mask != 0 {
+            let sq = pop_bit(&mut mask);
+            if i & (1 << bit) != 0 {
+                set_bit(comb, sq);
+            }
+            bit += 1
+        }
+    }
+
+    combs
+}
+
+fn init_blocker_boards() -> [u64; 36] {
+    let mut bbs = [0; 36];
+    for (i, bb) in bbs.iter_mut().enumerate() {
+        let mut buf;
+
+        // up
+        buf = 0;
+        for j in (i+6..36).step_by(6) {
+            if buf == 0 {
+                buf = j;
+            } else {
+                set_bit(bb, j);
+                set_bit(bb, buf);
+                buf = 0;
+            }
+        }
+
+        // right
+        buf = 0;
+        for j in i+1..((i / 6 + 1) * 6) {
+            if buf == 0 {
+                buf = j;
+            } else {
+                set_bit(bb, j);
+                set_bit(bb, buf);
+                buf = 0;
+            }
+        }
+
+        // down
+        if i > 11 {
+            buf = 0;
+            for j in (0..i-5).rev().step_by(6) {
+                if buf == 0 {
+                    buf = j;
+                } else {
+                    set_bit(bb, j);
+                    set_bit(bb, buf);
+                    buf = 0;
+                }
+            }
+        }
+
+        // left
+        buf = 0;
+        for j in (i/6*6..i).rev() {
+            if buf == 0 {
+                buf = j;
+            } else {
+                set_bit(bb, j);
+                set_bit(bb, buf);
+                buf = 0;
+            }
+        }
+
+        del_bit(bb, i);
     }
 
     bbs
--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,9 @@
+const WHITE: u64 = 0b101010010101101010010101101010010101;
+const BLACK: u64 = 0b010101101010010101101010010101101010;
+
+
+// main operations
+
 #[inline]
 pub fn get_bit(bitboard: u64, bit: usize) -> u64 {
     bitboard & (1 << bit)
@@ -13,6 +19,31 @@ pub fn del_bit(bitboard: &mut u64, bit: usize) {
     *bitboard &= !(1 << bit);
 }
 
-pub fn print_bitboard(human_readable: bool) {
-    
+#[inline]
+pub fn gtz(bitboard: u64) -> usize {
+    u64::trailing_zeros(bitboard) as usize
+}
+
+// return trailing zeros, then remove last bit
+#[inline]
+pub fn pop_bit(bitboard: &mut u64) -> usize {
+    let bit = gtz(*bitboard);
+    *bitboard &= *bitboard - 1;
+    bit
+}
+
+
+// other aappliances
+
+pub fn print_bitboard(bitboard: u64) {
+    for rank in (0..6).rev() {
+        for file in 0..6 {
+            print!("{} ", (bitboard >> (rank * 6 + file)) & 1 );
+        }
+        println!();
+    }
+}
+
+pub fn bb_to_str(bitboard: u64) -> String {
+    format!("{bitboard:036b}")
 }
